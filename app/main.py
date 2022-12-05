efd12ec17d094dd4013c056601b72fd618655527
David Gallmeier <dg@xounges.net>
2022-12-04 23:50:39 -0500

    Fix startup error

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def startup_event():
     is_uvicorn = True if "uvicorn" in sys.argv[0] else False
 
-    host_and_port = parse_uvicorn(sys.argv) if is_uvicorn else []
+    host_and_port = parse_uvicorn(sys.argv) if is_uvicorn else ("127.0.0.1", "8000")
     swaggerui_message = "View SwaggerUI at http://{host}:{port}/docs" \
         .format(host=host_and_port[0], port=host_and_port[1])
     redoc_message = "View Redoc at http://{host}:{port}/redoc" \
