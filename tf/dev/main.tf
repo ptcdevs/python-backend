52b3bb05245b0b36cbb6ace8de53f2408ecbe68b
David Gallmeier <dg@xounges.net>
2022-12-05 15:06:22 -0500

    Save changes

--- tf/dev/main.tf
+++ tf/dev/main.tf
@@ -1,26 +1,3 @@
-data local_file ptcdevs-kubectl {
-    filename = "../../config/ptcdevk8s-kubeconfig.yaml"
-}
-
-locals {
-    ptcdevk8s-kubeconfig = yamldecode(base64decode(data.linode_lke_cluster.ptcdevk8s.kubeconfig))
-    k8s = {
-        auth                 = {
-            cluster-ca-cert = base64decode(local.ptcdevk8s-kubeconfig.clusters[0].cluster.certificate-authority-data)
-            endpoint        = local.ptcdevk8s-kubeconfig.clusters[0].cluster.server
-            token           = local.ptcdevk8s-kubeconfig.users[0].user.token
-        }
-    }
-    ingress-nginx-namespace = "ingress-nginx"
-    ghcrcreds            = {
-        "ghcr.io" = {
-            email    = "dg@xounges.net"
-            username = "vector623"
-            password = var.GITHUB_TOKEN
-        }
-    }
-}
-
 provider linode {
     token = var.LINODE_TOKEN
 }
@@ -34,15 +11,18 @@ provider kubernetes {
     host                   = local.k8s.auth.endpoint
     cluster_ca_certificate = local.k8s.auth.cluster-ca-cert
     token                  = local.k8s.auth.token
+
 }
 
 data kubernetes_namespace dev {
+    provider = kubernetes.ptcdevs
     metadata {
         name = "dev"
     }
 }
 
 resource kubernetes_secret ghcr-secret {
+    provider = kubernetes.ptcdevs
     metadata {
         name      = "ghcr-secret"
         namespace = data.kubernetes_namespace.dev.metadata[0].name
